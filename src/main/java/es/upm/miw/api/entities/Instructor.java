7dd88db46c741488dc4b636a1683a710c17b8134
danhuang <mikdan89@gmail.com>
2017-10-12 14:32:22 +0200

    commit entities

---
+++ src/main/java/es/upm/miw/api/entities/Instructor.java
@@ -0,0 +1,69 @@
+
+package es.upm.miw.api.entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class Instructor {
+    
+    private int id;
+    private String firstName;
+    private String lastName;
+    private Calendar hireDate;
+    
+    public Instructor(int id, String firstName){
+        this.id = id;
+        this.firstName = firstName;
+        this.hireDate = Calendar.getInstance();
+    }
+       
+    public Instructor(int id, String firstName, String lastName) {
+        super();
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.hireDate = Calendar.getInstance();
+    }
+
+    public int getId() {
+        return id;
+    }
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public String getFirstName() {
+        return firstName;
+    }
+    
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    
+    public String getLastName() {
+        return lastName;
+    }
+    
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    
+    public Calendar getHireDate() {
+        return hireDate;
+    }
+    
+    public void setHireDate(Calendar hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    @Override
+    public String toString() {
+        
+        String formattedDate = new SimpleDateFormat("HH:00 dd-MMM-yyyy ").format(hireDate.getTime());
+        return "Instructor [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", hireDate=" + formattedDate + "]";
+    }
+    
+    
+
+}
