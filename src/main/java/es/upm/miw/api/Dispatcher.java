6c197d8f29cfd7fff9ab10f8234d8b390c472d30
danhuang <mikdan89@gmail.com>
2017-10-13 01:45:27 +0200

    add constructor

--- src/main/java/es/upm/miw/api/Dispatcher.java
+++ src/main/java/es/upm/miw/api/Dispatcher.java
@@ -1,5 +1,14 @@
 package es.upm.miw.api;
 
+import es.upm.miw.api.resources.CourseResource;
+import es.upm.miw.api.resources.InstructorResource;
+
 public class Dispatcher {
 
+    private InstructorResource themeResource = new InstructorResource();
+
+    private CourseResource voteResource = new CourseResource();
+
+    
+    
 }
