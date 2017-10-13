862208d1156062903ff21c6f04811b6fcf7b04ce
danhuang <mikdan89@gmail.com>
2017-10-13 02:09:06 +0200

    add exception

---
+++ src/main/java/es/upm/miw/http/HttpException.java
@@ -0,0 +1,11 @@
+package es.upm.miw.http;
+
+public class HttpException extends RuntimeException{
+    
+    private static final long serialVersionUID = 7271728903247259709L;
+
+    public HttpException(String msg) {
+        super(msg);
+    }
+
+}
