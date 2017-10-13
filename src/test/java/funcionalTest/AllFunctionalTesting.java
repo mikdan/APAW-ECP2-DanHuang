839e3fe75eca308e49d778b6d3eca2a6bfe70b49
danhuang <mikdan89@gmail.com>
2017-10-13 13:20:51 +0200

    commit alltest

---
+++ src/test/java/funcionalTest/AllFunctionalTesting.java
@@ -0,0 +1,15 @@
+package funcionalTest;
+
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+@RunWith(Suite.class)
+@SuiteClasses({
+    CourseResourceFuntionalTesting.class, 
+    InstructorRecourseFuntionalTesting.class
+})
+
+public class AllFunctionalTesting {
+
+}
